7f8d3044428f00760346974c012f372027b94ad5
pfrex <noahtock@yahoo.com>
2019-10-27 10:13:43 -0500

    Getting repo set up.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// my imports
+import Navbar from "./Navbar";
+import BlogPost from './blogPost';
+//import Footer from './Footer';// for copyright info
+// also add a button that will take them to the top of the page if they scroll
+
+
+// this is where I should compose all of my elements
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <Navbar/>
+    <BlogPost/>
+    <BlogPost/> 
+    <BlogPost/>
+    <BlogPost/>
+    <BlogPost/>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default App; 
+
+
+// the blog posts will be loaded dynamically from Firebase
+// or should I use Redis or just write everything to a local file? 
+// or implement a firestore document save solution? 
+// use firebase storage for files and firestore for post? How could I associate the files in firebase storage to there corresponding firebase firestore content? 
+
+// use drag and drop for the content and the images!
+// remove the form content and instead add file upload.
+// then the user can upload any type of text or image or video data 
+// what about mobile users? maybe eventually add a text editor to allow formatting on mobile? 
+// add any type of file and display it on the webpage somehow.
+
+
+// use firestore fr the posts and storage for the videos and images
---
+++ src/blogPost.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+let post = "aergaerg er aerg ruig aiergh eriugh aieurg lskdjfh sd  dsk ewui aiweufh iauwer  ewiaf oaweif aerig aeroig oerg eroig eori oaier oaeirg oae;irjg ;aeoergaerg esr er rsth rth wrt lskdjfh sd  dsk ewui aiweufh iauwer  ewiaf oaweif aerig aeroig oerg eroig eori oaier oaeirg oae;irjg ;aeoergaerg esr er rsth rth wrt lskdjfh sd  dsk ewui aiweufh iauwer  ewiaf oaweif aerig aeroig oerg eroig eori oaier oaeirg oae;irjg ;aeoergaerg esr er rsth rth wrt lskdjfh sd  dsk ewui aiweufh iauwer  ewiaf oaweif aerig aeroig oerg eroig eori oaier oaeirg oae;irjg ;aeoergaerg esr er rsth rth wrt lskdjfh sd  dsk ewui aiweufh iauwer  ewiaf oaweif aerig aeroig oerg eroig eori oaier oaeirg oae;irjg ;aeoergaerg esr er rsth rth wrt";
+
+function BlogPost() {
+
+    // may use a styled hr element to draw lines that will separate blog posts instead of using a separat background color
+    return(
+        <div className="shadow-lg p-3 m-5 bg-primary rounded">
+        <div className="text-left m-2">
+            <p>{"Oct 25th 2019"}</p>
+            <h5 className="text-center">{"Title"}</h5>
+            <div>{post}</div>
+            <div className="mt-5">
+                {"images will go here."}
+            </div>
+        </div>
+        </div>
+    );
+
+
+}
+
+
+
+export default BlogPost;
