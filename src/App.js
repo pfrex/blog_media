ca891606d3e506c7ada1907e6cb308cff8079786
pfrex <noahtock@yahoo.com>
2019-10-31 20:05:47 -0500

    got the uploading working with hooks

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-// my imports
+import React from "react";
+import "./App.css";
 import Navbar from "./Navbar";
-import BlogPost from './blogPost';
-//import Footer from './Footer';// for copyright info
-// also add a button that will take them to the top of the page if they scroll
+import BlogPost from "./BlogPost";
+import FileUpload from "./FileUpload";
+import "./index.css";
 
-
-// this is where I should compose all of my elements
 function App() {
   return (
     <React.Fragment>
-    <Navbar/>
-    <BlogPost/>
-    <BlogPost/> 
-    <BlogPost/>
-    <BlogPost/>
-    <BlogPost/>
+      <Navbar />
+      <FileUpload />
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
     </React.Fragment>
   );
 }
 
-export default App; 
-
-
-// the blog posts will be loaded dynamically from Firebase
-// or should I use Redis or just write everything to a local file? 
-// or implement a firestore document save solution? 
-// use firebase storage for files and firestore for post? How could I associate the files in firebase storage to there corresponding firebase firestore content? 
-
-// use drag and drop for the content and the images!
-// remove the form content and instead add file upload.
-// then the user can upload any type of text or image or video data 
-// what about mobile users? maybe eventually add a text editor to allow formatting on mobile? 
-// add any type of file and display it on the webpage somehow.
-
-
-// use firestore fr the posts and storage for the videos and images
+export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,62 +1,50 @@
 import React from "react";
-//import "./navStyles.css";
-//import imgSrc from "./bike_bar.png";
 
-
-// get this looking good on mobile, remove the picture if neccesary 
 let content = "Welcome to Nathan's blog";
 
-console.log("This content is different from the master branch.");
-console.log("This should be pushing to the add_form branch.");
-
-console.log("The pull operation worked, now how to push to a feature branch?");
-
-console.log("Before dinner push to feature branch.");
-
 function Navbar() {
-    return (
-      <nav className="p-3 bg-primary text-black text-center">
-      <h1 style={{cursor:"pointer"}}><a className="text-white" href="index.html" style={{textDecoration:"none"}}>{content}</a></h1>
+  return (
+    <nav className="p-3 bg-primary text-black text-center">
+      <h1 style={{ cursor: "pointer" }}>
+        <a
+          className="text-white"
+          href="index.html"
+          style={{ textDecoration: "none" }}
+        >
+          {content}
+        </a>
+      </h1>
       <div className="row text-center p-3">
-           <h4 className="col" style={{cursor:"pointer"}}><a className="text-dark" href="index.html" style={{textDecoration:"none"}}>{"About"}</a></h4>
-           <h4 className="col" style={{cursor:"pointer"}}><a className="text-dark" href="index.html" style={{textDecoration:"none"}}>{"Products"}</a></h4>
-           <h4 className="col" style={{cursor:"pointer"}}><a className="text-dark" href="index.html" style={{textDecoration:"none"}}>{"Contact"}</a></h4>
+        <h4 className="col" style={{ cursor: "pointer" }}>
+          <a
+            className="text-dark"
+            href="index.html"
+            style={{ textDecoration: "none" }}
+          >
+            {"About"}
+          </a>
+        </h4>
+        <h4 className="col" style={{ cursor: "pointer" }}>
+          <a
+            className="text-dark"
+            href="index.html"
+            style={{ textDecoration: "none" }}
+          >
+            {"Products"}
+          </a>
+        </h4>
+        <h4 className="col" style={{ cursor: "pointer" }}>
+          <a
+            className="text-dark"
+            href="index.html"
+            style={{ textDecoration: "none" }}
+          >
+            {"Contact"}
+          </a>
+        </h4>
       </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
+    </nav>
+  );
+}
 
-  // ok I am definetly over thinking this get a decent app with great functionality built
-  // focus on the back end? 
-
-/*
-
-
-<a class="navbar-brand mb-0 h1" href="#">{content}</a>
-           <div>
-           <a class="navbar-brand mb-0 h1" href="#">{"About"}</a>
-           <a class="navbar-brand mb-0 h1" href="#">{"Products"}</a>
-           <a class="navbar-brand mb-0 h1" href="#">{"Contact"}</a>
-           </div>
-
-
-// get this looking good on mobile, remove the picture if neccesary 
-let content = "Welcome to Nathan's blog";
-
-function Navbar() {
-    return (
-      <div className="navBar">
-           <h1 className="interSpace">{content}</h1>
-           
-      </div>
-    );
-  }
-  */
-  /*
-        <div>
-              {content}
-              <img class="imgClass" src={imgSrc} alt="Bike on road that leads through a forest." width="128" height="128"></img>
-          </div>
-          */
+export default Navbar;
